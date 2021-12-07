cf3b10910d5b289b8139118dfa7707941902601a
salifou-root163 <50051019+salifou-root163@users.noreply.github.com>
2021-12-07 16:38:55 +0100

    Création des fonctions Help et Create
    - la fonction help permet de lister les fonctionnalités du script
    
    - la fonction create permet de creer le site web

---
+++ files/fonctions/create.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+#debut de la fonction de dreation du site
+
+function create {
+  if [ -e "web.html" ]; then
+   rm web.html
+  fi  
+  touch web.html  
+  cat files/header.txt >> web.html  
+
+  declare -a extensions=( "JPG" "jpg" "jpeg" "JPEG" "PNG" "png" )
+  for f in  images/*.*
+  do
+    fichier="$f"
+    ext=${fichier#*.}
+    if [[ " ${extensions[*]} " =~ $ext ]]; then      
+      echo "<div class='col col-md-4' >" >> web.html            
+      echo "<img style='height: 300px; width: 300px;margin: 10px;'  src='""$fichier""'>" >> web.html  
+      echo "</div>" >> web.html  
+      echo "<br>" >> web.html  
+    fi  
+  done
+
+  echo "</div>
+        <br>
+      <h1>les articles</h1>
+      <div class='container row'>">>web.html
+
+      nb_ligne=$(wc -l < files/data.csv)
+      declare -a tab_article
+      declare -a tab_photo
+      for ((i=1; i<=$nb_ligne; i++)) ;
+      do 
+      tab_article[$i]=$(awk 'BEGIN{FS=";";OFS="\n"} FNR=='$i'{print $1;exit}' files/data.csv)
+      tab_photo[$i]=$(awk 'BEGIN{FS=";";OFS="\n"} FNR=='$i'{print $2;exit}' files/data.csv)
+      done      
+
+      for ((i=1; i<=$nb_ligne; i++)) ;
+      do
+        echo "<div class='col col-md-4' >">>web.html        
+
+        echo "<img style='height: 300px; width: 300px;margin: 10px;'  src='""files/articles/images/${tab_photo[$i]}""'>" >> web.html 
+
+        echo "<p  style=' width: 300px;margin: 10px;'>">>web.html
+        cat "files/articles/${tab_article[$i]}" >> web.html
+        echo "</p>">>web.html
+        echo "</div>">>web.html
+        echo "<br>">>web.html
+      
+
+      done    
+
+
+  echo "</div>">>web.html
+
+  cat files/footer.txt >> web.html 
+}
+
+#fin de la fonction de creation du site
+
+#appel de la fonction
+
+create
---
+++ files/fonctions/help.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#debut de la fonction help
+
+function help {
+
+  echo "Bonjour, ce guide vous permettra d'utiliser aisement notre script"
+  echo ""
+  echo "notre script permet de generer un petit blog html "
+  echo ""
+  echo "- ./script.sh permet d'executer le script "
+  echo ""
+  echo "- ./script.sh --debug permet d'afficher le debug "
+  echo ""
+  echo "- ./script.sh build -target- permet de faire le build du projet dans le dossier de votre choix"
+  echo ""
+  echo "- ./script.sh add_image -le chemain de l'image- permet d'ajouter une image au blog"
+  echo ""
+  echo "- ./script.sh add_article -le chemain de l'article- permet d'ajouter un article"
+  echo ""
+  echo "- ./script.sh add_article -le chemain de l'article- add_image -le chemain de l'image- permet d'ajouter un article lié à une image"
+  echo ""
+  echo "- ./check.sh permet de verifier (ou recré) au besoin les fichiers necessaires pour le bon fonctionnement du script."
+
+}
+
+#fin de la fonction help
+
+#appel à la fonction
+
+help
