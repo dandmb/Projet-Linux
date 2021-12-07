cbaeebb65772c950f526cb094f8f57373325db4f
Wissam Ouhib <wissem.ouhib@gmail.com>
2021-12-07 17:14:11 +0100

    fonction add_article V1

---
+++ files/fonctions/add_article.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# fonction d'ajout d'un article
+
+function add_article { 
+  article=$ARTICLE_PATH
+  
+  if [ -e "$article" ];then
+    ext=${article##*.}
+    declare -a extensions=( "txt" "csv" )
+      if [[ " ${extensions[*]} " =~ $ext ]]; then
+        cp "$ARTICLE_PATH" files/articles/
+        nom_fichier=$(basename "${1}")
+        echo "$nom_fichier;null">>files/data.csv
+        echo "Article ajouté. "
+      else
+        echo "Veuillez mettre le lien d'un fichier txt ou csv"
+      fi
+    else
+    echo "Fichier $ARTICLE_PATH introuvable"
+  fi
+}
+
+#fin de la fonction d'ajout
+
+#appel à la fonction
+
+add_article
