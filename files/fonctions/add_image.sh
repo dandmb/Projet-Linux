637fcf3b22ae89fd34c9fb0d376de2c00aaf891f
Dan bizwa <dbizwa@Dans-MacBook-Pro.local>
2021-12-09 18:21:36 +0100

    modification de la fonction add_image

--- files/fonctions/add_image.sh
+++ files/fonctions/add_image.sh
@@ -1,16 +1,26 @@
-function add_image { 
-  img=$1
+#!/bin/bash
+
+# fonction d'ajout d'une image
+
+function add_image {
+  img=$IMG_PATH
   if [ -e "$img" ];then
     ext=${img#*.}
     declare -a extensions=( "JPG" "jpg" "jpeg" "JPEG" "PNG" "png" )
       if [[ " ${extensions[*]} " =~ $ext ]]; then
-        cp "$1" "$2"
+        cp "$IMG_PATH" "$IMGDIR_PATH"
         echo "Photo ajoutée."
       else
         echo "Veuillez mettre le lien d'une image de type PNG,JPEG ou JPG"
       fi
     else
-    echo "Fichier $1 introuvable"
+    echo "Fichier $IMG_PATH introuvable"
     exit 0
   fi
 }
+
+#fin de la fonction d'ajout
+
+#appel de la fonction
+
+add_image
