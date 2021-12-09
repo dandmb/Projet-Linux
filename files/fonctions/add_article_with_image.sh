b02090869c28f65055b7f9683c951e5a3da6fc8b
wissem <wissem.ouhib@gmail.com>
2021-12-09 22:34:32 +0100

    modification add_article_with_image condition tout ou rien

--- files/fonctions/add_article_with_image.sh
+++ files/fonctions/add_article_with_image.sh
@@ -2,34 +2,43 @@
 
 # fonction d'ajout d'un article lié à une image
 
-function add_article_with_img { 
+function add_article_with_img {
   article=$ARTICLE_PATH
   
   if [ -e "$article" ];then
     ext=${article##*.}
     declare -a extensions=( "txt" "csv" )
       if [[ " ${extensions[*]} " =~ $ext ]]; then
+
+        #On commence par essayer d'ajouter l'image , s'il y a un souci, on arrête tout, sinon (image rajoutée correctement) on rajoute l'article
+
+        # add_image "$2" "$3"
+
+        if [ -e "files/fonctions/add_image.sh" ];then
+        /bin/bash files/fonctions/add_image.sh
+       
+       retval=$?
+       if [ "$retval" == 0 ]; then
+        exit 0
+        else :
+       fi
+
+       else
+         echo "Fichier add_image.sh introuvable"
+       exit 0
+       fi 
+
         cp "$ARTICLE_PATH" files/articles/
         nom_fichier=$(basename "${ARTICLE_PATH}")
         nom_image=$(basename "${IMG_PATH}")
         echo "$nom_fichier;$nom_image">>files/data.csv
         echo "Article ajouté. "
 
-       # add_image "$2" "$3"
-        
-
-        if [ -e "files/fonctions/add_image.sh" ];then
-        /bin/bash files/fonctions/add_image.sh
-       else
-         echo "Fichier add_image.sh introuvable"
-       exit 0
-       fi 
-
       else
         echo "Veuillez mettre le lien d'un fichier txt ou csv"
       fi
     else
-    echo "Fichier $IMG_PATH introuvable"
+    echo "Fichier $ARTICLE_PATH introuvable"
   fi
 }
 
