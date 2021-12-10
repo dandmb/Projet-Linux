45c7c7755fc2566e01b0daca09b95995d119f602
wissem <wissem.ouhib@gmail.com>
2021-12-10 11:28:25 +0100

    correction create.sh

--- files/fonctions/create.sh
+++ files/fonctions/create.sh
@@ -30,8 +30,6 @@ function create {
       nb_ligne=$(wc -l < files/data.csv)
       declare -a tab_article
       declare -a tab_photo
-      lien_article="files/articles/"
-      lien_image="files/articles/images/"
       for ((i=1; i<=$nb_ligne; i++)) ;
       do 
       tab_article[$i]=$(awk 'BEGIN{FS=";";OFS="\n"} FNR=='$i'{print $1;exit}' files/data.csv)
@@ -40,12 +38,15 @@ function create {
 
       for ((i=1; i<=$nb_ligne; i++)) ;
       do
-        echo "<div class='col col-md-4' >">>web.html        
+        echo "<div class='col col-md-4' >">>web.html  
 
-        echo "<img style='height: 300px; width: 300px;margin: 10px;'  src='""${lien_image}${tab_photo[$i]}""'>">>web.html 
+      if [ ${tab_photo[$i]}==null ];then :
+       else 
+        echo "<img style='height: 300px; width: 300px;margin: 10px;'  src='""files/articles/images/${tab_photo[$i]}""'>">>web.html 
+       fi
 
         echo "<p  style=' width: 300px;margin: 10px;'>">>web.html
-        cat "${lien_article}${tab_article[$i]}">>web.html
+        cat "files/articles/${tab_article[$i]}" >> web.html
         echo "</p>">>web.html
         echo "</div>">>web.html
         echo "<br>">>web.html
